7d253c7e7c7474007138f4930f88427c8e45e371
Christopher O'Brien <chrisobrien5823@gmail.com>
2026-04-09 11:46:24 -0400

    Hopefully fixed the logs bug

---
+++ Sprint_1/static/getSubmissionCode.js
@@ -0,0 +1,47 @@
+import socket from "./sockets/getSocketBackend.js";
+import { getTerminal } from "./services/initTerminal.js";
+
+export default function getSubmissionCode() {
+    console.log("Clicked submission.");
+    let compileButton = document.querySelector(".compile");
+    
+
+    // Determine button type
+    if(!compileButton)
+    {
+        compileButton = document.querySelector(".stop-button");
+    }
+
+    // If the user presses the run button
+    if(compileButton.textContent.includes("Run"))
+    {
+        // Get the code and add to local storage
+        const code = window.monacoEditor.getValue();
+        localStorage.setItem("code", code);
+
+        // Clear terminal
+        getTerminal()?.clearTerminal();
+
+        // Change button to stop
+        compileButton.textContent = "⏹ Stop";
+        compileButton.classList.add("stop-button");
+        compileButton.classList.remove("compile");
+
+        // Emit the compile event with the code
+        console.log("Code emitted: ", code);
+        socket.emit("compile", { code: code });
+    }
+    else
+    {
+        // Stopping execution
+        getTerminal().writeToTerminal("\n\x1b[1;31mProcess Stopped.\x1b[0m") // write in red
+        socket.emit("stop_process");
+    }
+}
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("compile") || e.target.classList.contains("stop-button")) 
+    {
+        getSubmissionCode();
+    }
+});
